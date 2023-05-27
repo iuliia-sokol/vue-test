86e22a5844d9d139de6302d04548f93f23b258c4
Iuliia Sokol <avare779@gmail.com>
2023-05-27 20:47:29 +0300

    added deadline indication

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -13,7 +13,10 @@
        </button>
       </div>
       <div class="card__content">
-        <p class="card__deadline" v-if="card.deadline !== ''">{{ card.deadline }}</p>
+        <p class="card__description"  v-if="card.body !== ''">{{ card.body }}</p>
+        <p class="card__deadline" 
+         :style= "checkDeadline? 'background-color: red;' : 'background-color: green;'"
+         v-if="card.deadline !== ''">{{ card.deadline }}</p>
       </div>
     </li>
 
@@ -28,16 +31,29 @@
         type: Object,
         required: true, 
       },
-      cards: {
-      type: Array,
-      default: ()=>[],
-      required: true
-    },
+        cards: {
+        type: Array,
+        default: ()=>[],
+        required: true
+      },
     },
     data(props) {
     return {
       cardCurrent: props.card,
     }},
+    computed:{
+      checkDeadline(){
+        if(this.card.deadline){
+        const date = Date.now()
+        const today = new Date(this.card.deadline).valueOf() 
+        if(date<today){
+          return false
+        }
+        return true
+      }
+        return true
+      }
+    },
     methods: {
     startDrag(e, item) {
       e.dataTransfer.dropEffect = 'move'
@@ -45,10 +61,6 @@
       e.dataTransfer.setData('itemId', item.id.toString())
     },
     },
-
-  
-    
-
     }
    
 </script>
@@ -80,10 +92,17 @@
     border: none;
     padding: 8px;
 }
+.card__description{
+  font-size: x-small;
+  overflow: hidden;
+    white-space: nowrap;
+    max-width: 80%;
+    text-overflow: ellipsis;
+}
 .card__deadline {
     display: flex;
     justify-content: center;
-    padding: 8px;
+    padding: 6px;
     border-radius: 8px;
     background-color: yellow;
     width: fit-content;
